42b30e3b9230fc3970f74b98792b2ff87897558c
Thomas Nilefalk <thomas@nilefalk.se>
2020-05-10 22:50:34 +0200

    Testdrive collating data for one occasion for one source file

--- lcov_to_gcov_fake.py
+++ lcov_to_gcov_fake.py
@@ -1,2 +1,41 @@
-def open_info_file(file_name):
-    return open(file_name, "r")
+#! /usr/bin/env python3
+
+import argparse
+
+
+def collate(info_content, filename):
+    data = {}
+    collecting = False
+    for info_line in info_content:
+        if not collecting:
+            if info_line.startswith("SF:") and info_line.endswith("/"+filename):
+                collecting = True
+        else:
+            if info_line.startswith("DA:"):
+                [line, executions] = info_line.split(":")[1].split(",")
+                add_execution_data_for_line(data, (int(line), int(executions)))
+            else:
+                if info_line.startswith("SF:"):
+                    collecting = False
+    return data
+
+
+def add_execution_data_for_line(data, line_data):
+    (line, executions) = line_data
+    try:
+        data[line] = executions + data[line]
+    except KeyError:
+        data[line] = executions
+
+
+if (__name__ == "__main__"):
+
+    argparser = argparse.ArgumentParser(
+        description='Read an info file from lcov and convert it to aggregated single files or all source files in it')
+    argparser.add_argument(
+        'inputfile', help='name of the info file from lcov')
+
+    args = argparser.parse_args()
+
+    with open(args.inputfile) as f:
+        collate(f.readlines())
--- lcov_to_gcov_fake_test.py
+++ lcov_to_gcov_fake_test.py
@@ -1,25 +1,52 @@
 import unittest
+import unittest.mock
 
-from lcov_to_gcov_fake import open_info_file
-
-import os
+from lcov_to_gcov_fake import collate, add_execution_data_for_line
 
 
 class TestLcovToGcov(unittest.TestCase):
 
-    def test_open_info_file(self):
-        with open("test.info", "w") as f:
-            f.write("TN:test_add_parameter")
-            f.write("SF:/home/thoni/Utveckling/c-xrefactory/src/c_parser.y")
-            f.write("FN:1823,exists_valid_parser_action_on")
-            f.write("FN:1838,makeCCompletions")
-            f.write("FNDA:0,makeCCompletions")
-            f.write("FNDA:0,exists_valid_parser_action_on")
-            f.write("FNF:2")
-            f.write("FNH:0")
-            f.write("DA:251,36")
-        try:
-            f = open_info_file("test.info")
-        except IOError as e:
-            self.fail()
-        os.remove("test.info")
+    def test_collates_one_execution_for_one_line_with_single_file_in_info(self):
+        info_content = [
+            "TN:test_addparameter",
+            "SF:/home/thoni/Utveckling/c-xrefactory/src/this_file.c",
+            "FN:1823,exists_valid_parser_action_on",
+            "FN:1838,makeCCompletions",
+            "FNDA:0,makeCCompletions",
+            "FNDA:0,exists_valid_parser_action_on",
+            "FNF:2",
+            "FNH:0",
+            "DA:251,36"
+        ]
+        expected = {251: 36}
+        result = collate(info_content, "this_file.c")
+        self.assertEqual(result, expected)
+
+    def test_collates_one_execution_for_one_line_with_multiple_files_in_info(self):
+        info_content = [
+            "TN:test_add_parameter",
+            "SF:/home/thoni/Utveckling/c-xrefactory/src/not_this_file.c",
+            "SF:/home/thoni/Utveckling/c-xrefactory/src/this_file.c",
+            "FN:1823,exists_valid_parser_action_on",
+            "FN:1838,makeCCompletions",
+            "FNDA:0,makeCCompletions",
+            "FNDA:0,exists_valid_parser_action_on",
+            "FNF:2",
+            "FNH:0",
+            "DA:251,36"
+        ]
+        expected = {251: 36}
+        result = collate(info_content, "this_file.c")
+        self.assertEqual(result, expected)
+
+    def test_can_add_executions_to_empty_line_data(self):
+        data = {}
+        expected = {251: 44}
+        add_execution_data_for_line(data, (251, 44))
+        self.assertEqual(data, expected)
+
+    def test_can_add_executions_to_existing_line_data(self):
+        data = {251: 22}
+        expected = {251: 66}
+        add_execution_data_for_line(data, (251, 44))
+        self.assertEqual(data, expected)
